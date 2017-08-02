ea6b933c9a46486b5e91d90799839df23a94f0fa
MichaelCook <michael@evoluted.net>
2017-08-02 22:54:07 +0100

    That'll do, pig. That'll do.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# chiptune-player
-.XM (maybe .mod) player via jsxm or others..
+# Chiptune player
+
+.XM player in JS via (jsxm)[https://github.com/a1k0n/jsxm], simple as possible. Should be a cracking way to play BGM in games.
+
+## Run
+
+    npm i
+    npm start
+
+## Note
+
+Hopefully they'll accept my request for npm publishing to do away with the .git specific package
--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-// var mod = new Protracker();
-// mod.load('arvids.mod');
-// mod.autostart = true;
-//
-//
-
 XMPlayer.init();
 
-var uri = 'jt_mind.xm';
+var uri = 'test.xm';
 
 var xmReq = new XMLHttpRequest();
 xmReq.open('GET', uri, true);
 xmReq.responseType = 'arraybuffer';
 xmReq.onload = function(xmEvent) {
-    var arrayBuffer = xmReq.response;
-    if (arrayBuffer) {
-        XMPlayer.load(arrayBuffer); // ArrayBuffer
-        XMPlayer.play(); // pause() stop()
-    } else {
-        console.log('unable to load', uri);
-    }
+  var arrayBuffer = xmReq.response;
+  if (arrayBuffer) {
+    XMPlayer.load(arrayBuffer); // ArrayBuffer
+    XMPlayer.play(); // pause() stop()
+  } else {
+    console.log('unable to load', uri);
+  }
 };
+
 xmReq.send(null);
